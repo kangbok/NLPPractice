9de7d123185e519b94815fbd5e144ec6ef24f280
kangbok <kangbok.kbl@gmail.com>
2018-11-27 23:39:43 +0900

    wordvectorcomparing update

--- Ex/WordVectorComparingEx.py
+++ Ex/WordVectorComparingEx.py
@@ -5,9 +5,12 @@ from glove import Glove
 g = Glove.load("../model/wordvector_glove.model")
 w2v = Word2Vec.load("../model/wordvector_w2v.model")
 
-word = "쓰레기/Noun"
+word = "클레멘타인/Noun"
+# word = "졸리다/Verb"
 
 print(g.most_similar(word, number=10))
 print("=" * 20)
 print(w2v.most_similar(word))
+
+print(w2v.wv.similarity("발연기/Noun", "명연기/Noun"))
 
